a4baab50be7e6a772c9f5c487a18dcb5b8f3a948
Serge van Namen <serge@se-cured.org>
2012-08-30 13:25:16 +0200

    initial setup

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+CUCKOO_PATH=""
+INETSIM_PATH=""
+TOR_CONFIG_PATH=""
---
+++ cuckoo_additions.py
@@ -0,0 +1,31 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+# Install argparse module trough PIP or install manually
+
+import sys
+import time
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser("Cuckoo Sandbox Additions")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-t", "--tor", help="Enable TOR Transparant Proxy", action="store_true", required=False)
+group.add_argument("-i", "--inetsim", help="Enable iNetsim", action="store_true", required=False)
+group.add_argument("-n", "--nat", help="Enable NAT", action="store_true", required=False)
+args = parser.parse_args()
+
+if args.tor:	
+	print "Enable TOR"
+	subprocess.call(["echo", "start TOR"])
+
+# Rare loop om er voor te zorgen dat proces blijft draaien, kan waarschijnlijk netter.
+# Zodra deze gestopt wordt zal alles weer normaal worden.
+
+while True:
+	try:
+		time.sleep(1)
+
+	except KeyboardInterrupt:
+		print "\nBack to defaults"
+		subprocess.call(["echo", "STOP ALLE SERVICES"])
+		sys.exit()
